60db30dcd591804e802abad03b243180f210bd3e
Damien Robert <damien.olivier.robert+git@gmail.com>
2015-02-23 18:06:31 +0100

    Add test

--- test/test_tiny_singleton.rb
+++ test/test_tiny_singleton.rb
@@ -3,10 +3,23 @@ require 'tiny_singleton'
 
 class TestTinySingleton < Minitest::Test
 
-  def test_version
-    version = TinySingleton.const_get('VERSION')
+	def test_version
+		version = TinySingleton.const_get('VERSION')
 
-    assert(!version.empty?, 'should have a VERSION constant')
-  end
+		assert(!version.empty?, 'should have a VERSION constant')
+	end
+
+	def test_singleton_pattern
+		fooClass=Class.new do
+			prepend TinySingleton
+			def initialize(foo)
+				@foo=foo
+			end
+		end
+		foo=fooClass.new("foo")
+		bar=fooClass.new("bar")
+		assert(foo.object_id==bar.object_id, 'Objects created by new should be identical')
+
+	end
 
 end
