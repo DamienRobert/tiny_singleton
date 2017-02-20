f4e89b7ced742b7f4baca2945bf3da2782bfe22c
Damien Robert <damien.olivier.robert+git@gmail.com>
2017-02-20 14:06:48 +0100

    test: use minitest reporters

--- test/helper.rb
+++ test/helper.rb
@@ -1,2 +1,13 @@
-require 'rubygems'
 require 'minitest/autorun'
+
+## Uncomment to launch pry on a failure
+#require 'pry-rescue/minitest'
+
+begin
+  require 'minitest/reporters'
+  Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
+  #Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+  #Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
+rescue LoadError => error
+  warn "minitest/reporters not found, not changing minitest reporter: #{error}"
+end
